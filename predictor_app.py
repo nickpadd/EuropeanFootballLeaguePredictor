48cca930b2a74b0f1fb09ff7f6eda3058ce000f7
nickpadd <nickpad1@hotmail.com>
2024-03-06 00:51:10 +0200

    Changing default leage

--- predictor_app.py
+++ predictor_app.py
@@ -1,5 +1,5 @@
 import gradio as gr
-from loguru import logger 
+from loguru import logger
 import argparse
 from europeanfootballleaguepredictor.common.config_parser import Config_Parser
 import pandas as pd
@@ -9,14 +9,14 @@ from pretty_html_table import build_table
 import importlib
 import os
 import argparse
-from europeanfootballleaguepredictor.data.database_handler import DatabaseHandler 
+from europeanfootballleaguepredictor.data.database_handler import DatabaseHandler
 
 
 def league_predictions_figure(league_name, regressor_str, matchdays_to_drop, long_form_vote):
-    """Generates a prediction table with the model predictions of certain match outcomes 
+    """Generates a prediction table with the model predictions of certain match outcomes
     and a prediction figure that includes a radar plot comparing the bookmakers odds depicted as probabilities with the predicted probabilities
-    as well as a barplot with the most probable scorelines according to the model. 
-    
+    as well as a barplot with the most probable scorelines according to the model.
+
     Args:
         league_name (str): One of the available leagues ['EPL', 'Bundesliga', 'Ligue_1', 'La_Liga', 'Serie_A']
         regressor_str (str): One of the available regressors ['LinearRegression', 'PoissonRegressor', 'SVR']
@@ -27,8 +27,8 @@ def league_predictions_figure(league_name, regressor_str, matchdays_to_drop, lon
         plotly_figure: A prediction figure that includes a radar plot comparing the bookmakers odds depicted as probabilities with the predicted probabilities as well as a barplot with the most probable scorelines according to the model.
         html_table: A prediction table with the model predictions of certain match outcomes.
     """
-    
-    voting_dict = {'long_term': long_form_vote, 'short_term': 1-long_form_vote} 
+
+    voting_dict = {'long_term': long_form_vote, 'short_term': 1-long_form_vote}
     '''Parsing the configuration file'''
     if (regressor_str == "LinearRegression") or (regressor_str == "PoissonRegressor"):
         module_path = 'sklearn.linear_model'
@@ -38,33 +38,33 @@ def league_predictions_figure(league_name, regressor_str, matchdays_to_drop, lon
         module_path = 'sklearn.svm'
         regressor_module = importlib.import_module(module_path)
         regressor = getattr(regressor_module, regressor_str)
-        
+
     parser = argparse.ArgumentParser()
     parser.add_argument("--config", type=str, help="Path to the configuration file (e.g., config.yaml)", default='europeanfootballleaguepredictor/config/config.yaml')
     config_file_path = parser.parse_args().config
-    
+
     config_data_parser = Config_Parser(config_file_path, None)
     config_data = config_data_parser.load_and_extract_yaml_section()
     config = config_data_parser.load_configuration_class(config_data)
-    
+
     logger.info(config)
     '''End of the configuration file parsing'''
-    
+
     pd.set_option('display.precision', 2)
-    
+
     net = ProbabilityEstimatorNetwork(voting_dict=voting_dict, matchdays_to_drop=matchdays_to_drop)
     net.build_network(regressor = regressor)
-        
+
     database = f'europeanfootballleaguepredictor/data/database/{league_name}_database.db'
     database_handler = DatabaseHandler(league=config.league, database=database)
     short_term_form, long_term_form, for_prediction_short, for_prediction_long = database_handler.get_data(table_names=["Preprocessed_ShortTermForm", "Preprocessed_LongTermForm", "Preprocessed_UpcomingShortTerm", "Preprocessed_UpcomingLongTerm"])
-    
+
     probability_dataframe = net.produce_probabilities(short_term_data=short_term_form, long_term_data=long_term_form, for_prediction_short=for_prediction_short, for_prediction_long=for_prediction_long)
 
     visualizer = Visualizer(probability_dataframe)
     figure = visualizer.radar_scoreline_plot()
     html_table = build_table(probability_dataframe.drop(['ScorelineProbability', 'Match_id'], axis=1), 'blue_light')
-    
+
     return figure, html_table
 
 def main():
@@ -86,6 +86,6 @@ def main():
         btn.click(league_predictions_figure, inputs=[drop1, drop3, slide1, slide2], outputs=[plot1, table1])
 
     iface.launch(height=2000)
-    
+
 if __name__ == "__main__":
     main()
